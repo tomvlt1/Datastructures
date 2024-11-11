409e4bb41a715923514fbd68e21e8c4258d80adc
Carlo Fialdini <cfialdini.ieu2023@student.ie.edu>
2024-11-11 18:58:56 +0100

    I have created functions for the entering of the information of the user, specifically names and, birthdate

--- UserInput.py
+++ UserInput.py
@@ -1,3 +1,18 @@
-def helo():
-    print("hello world")
+
+def UserName():
+    user_name = input("")
+
+def UserLastName():
+    last_name = input("")
+
+def UserBirthYear():
+    birth_year = int(input(""))
+
+def UserBirthDay():
+    birth_day= int(input(""))
+
+def UserBirthMonth():
+    birth_month= int(input(""))
+
+
 

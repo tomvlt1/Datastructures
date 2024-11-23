ce63f1b13e7979195a11de36deb38fca1e3dec09
Javier Cruz <jcruz.ieu2023@student.ie.edu>
2024-11-23 01:47:28 +0100

    more

--- app/LoadData.py
+++ app/LoadData.py
@@ -4,11 +4,7 @@ import pandas as pd
 
 #For collaborators I need to send the names of the fields and not send more than necessary
 
-data = pd.read_csv("generated_database.csv", header=None, names=[
-    "First Name", "Last Name", "Rating", "Age", "DOB", "Nationality",
-    "Country of Residence", "Degree", "Graduation Year", "GPA",
-    "Availability", "Topics of Interest", "Email", "Description",
-    "Additional Information", "LinkedIn","Sort Value"],low_memory=False)
+data = pd.read_csv("generated_database.csv")
 
 
 data['Age'] = pd.to_numeric(data['Age'], errors='coerce')

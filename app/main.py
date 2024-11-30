267a084ef23021856c3d7bbc8c000be587df4381
tom_vlt1 <tom.valentinuk@gmail.com>
2024-11-30 17:24:54 +0100

    added auto open when running main.py

--- app/main.py
+++ app/main.py
@@ -1,36 +1,38 @@
+import threading
+import webbrowser
+import os
+from flask import Flask, session, redirect, url_for
 from routes.Home_Routes import home_bp
 from routes.Collaborators_Routes import collaborators_bp
 from routes.Login import login_bp
 from routes.Profile import account_bp
 from routes.Mentors_Routes import mentors_bp
 from routes.Projects import projects_bp
-from flask import Flask,session, redirect, url_for
 
- 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '456ytu@w789'
-
 
 # Register blueprints
 app.register_blueprint(home_bp)
 app.register_blueprint(collaborators_bp, url_prefix='/collaborators')
 app.register_blueprint(login_bp, url_prefix='/login')
 app.register_blueprint(account_bp, url_prefix='/account')
-app.register_blueprint(mentors_bp,url_prefix ='/mentors')
-app.register_blueprint(projects_bp,url_prefix ='/projects')
-#la ruta /logout debe estar fuera de cualquier Blueprint para que sea accesible globalmente.
+app.register_blueprint(mentors_bp, url_prefix='/mentors')
+app.register_blueprint(projects_bp, url_prefix='/projects')
+
 
 @app.route('/logout')
-
 def logout():
+    session.clear()  
+    return redirect(url_for('home.home'))  
 
-    session.clear()  # Elimina todas las variables de la sesión
-
-    return redirect(url_for('home.home'))  # Redirige a la página de inicio
+def open_browser():
+    """Open the default web browser to the app's URL."""
+    webbrowser.open_new('http://127.0.0.1:5000/')
 
 if __name__ == '__main__':
-
+    
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        threading.Timer(1, open_browser).start()
     app.run(debug=True)
-
-   

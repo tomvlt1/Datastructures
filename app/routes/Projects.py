1d0c551316e42e22a22ba6b9689e1881ea5ba28d
guittoi <guitoigo2005@gmail.com>
2024-12-13 15:33:58 +0100

    Updated Projects.py

--- app/routes/Projects.py
+++ app/routes/Projects.py
@@ -1,3 +1,8 @@
+#this file is responsible to manage project-related functinalities
+#it includes routes for creating, updating, viewing, filtering as well as searching for projects.
+#this file also manages validation, sessino-based access control and finally storage and retrieving 
+#of project data from a csv
+
 from flask import Blueprint, render_template, request, redirect, session, url_for, jsonify
 from projectclass import Project
 from datetime import datetime
@@ -6,9 +11,10 @@ from Filter_projects import filter_projects,filter_data_fullname
 from Display import AddSortValueProjects
 import csv
 
-
 projects_bp = Blueprint('projects', __name__)
 
+# the function below should be able to administer the main projects page, process filter values,
+# and finally return filter or sorted projects 
 @projects_bp.route('/', methods=['GET', 'POST'])
 def projects():
   
@@ -48,7 +54,9 @@ def projects():
         
         # Return an error response if something goes wrong
         return jsonify({'error': 'An unexpected error occurred'}), 500
-    
+
+# the function below should be able to manage the user's personal project's page, 
+# as well as process filters and lastly display filtered or sorted data 
 @projects_bp.route('/Myprojects', methods=['GET', 'POST'])   
 def Myprojects():
     if session.get('IsLogged') != True:
@@ -93,7 +101,9 @@ def Myprojects():
         
         # Return an error response if something goes wrong
         return jsonify({'error': 'An unexpected error occurred'}), 500
-    
+
+# the function below has the objective to develop the functionality of creating a new projects
+# along with validating the given input and finally saveing the project
 @projects_bp.route('/create_project', methods=['GET', 'POST'])
 def create_project():
     if session.get('IsLogged') != True:
@@ -113,10 +123,10 @@ def create_project():
         project_description = request.form.get('project_description')
         positions_needed = request.form.get('positions_needed')  
        # Process keywords and positions_needed into lists
-            # Para keywords
+            # to keywords
         keywords_str = f"['{', '.join([keyword.strip() for keyword in keywords.split(',')])}']" if keywords else "[]"
         keywords_list= keywords_str
-        # Para positions_needed
+        # to positions_needed
         positions_needed_str = f"['{', '.join([position.strip() for position in positions_needed.split(',')])}']" if positions_needed else "[]"
         positions_needed_list=positions_needed_str
 
@@ -159,61 +169,57 @@ def create_project():
     else:
         return render_template('createProject.html',admin=session['email'])
 
+# the fucntion below should fetch and display details of a given project based on the project's id
 @projects_bp.route('/project/consult', methods=['POST'])
 def projectconsult():
-    project_id = request.form.get('id_project')  # Obtener el ID del proyecto desde el formulario
-
+    project_id = request.form.get('id_project')  
     if not project_id:
         print("Project not found.")
 
     try:
-        # Intentamos obtener los datos del proyecto usando el ID
-        project_id = int(project_id)  # Aseguramos que el ID es un número
-        project_data = Project.get_project_data_from_csv(project_id)  # Buscar los datos del proyecto
+        project_id = int(project_id)  
+        project_data = Project.get_project_data_from_csv(project_id)  
 
         if not project_data:
-            # Si no se encuentran los datos, podemos mostrar un mensaje o redirigir
             print("Project not found.")
 
-        # Si el proyecto se encuentra, renderizamos los datos
         return render_template('Project.html', project_data=project_data)
 
     except ValueError:
-        # Si ocurre un error al intentar convertir el ID en entero
         print("Invalid Project ID format.")
 
     except Exception as e:
-        # Cualquier otra excepción que pueda ocurrir
         print(e)
 
+# the function below should render the details of a given project for viewing without modification
 @projects_bp.route('/project/view', methods=['POST'])
 def viewproject():
-    project_id = request.form.get('id_project')  # Obtener el ID del proyecto desde el formulario
-
+    project_id = request.form.get('id_project') 
     if not project_id:
         print("Project not found.")
 
     try:
-        # Intentamos obtener los datos del proyecto usando el ID
-        project_id = int(project_id)  # Aseguramos que el ID es un número
-        project_data = Project.get_project_data_from_csv(project_id)  # Buscar los datos del proyecto
+        #trying to get the data of the project using the 
+        project_id = int(project_id)  #ensure that the id is an integer
+        project_data = Project.get_project_data_from_csv(project_id)  #search for the projects data
 
         if not project_data:
-            # Si no se encuentran los datos, podemos mostrar un mensaje o redirigir
+            #if the data is not found a message is printed
             print("Project not found.")
 
-        # Si el proyecto se encuentra, renderizamos los datos
+        #if the project is found the data is renderized 
         return render_template('viewproject.html', project_data=project_data)
 
     except ValueError:
-        # Si ocurre un error al intentar convertir el ID en entero
+        #in the case an error occurs when trying to convert the id into an integer
         print("Invalid Project ID format.")
 
     except Exception as e:
-        # Cualquier otra excepción que pueda ocurrir
+        #any other expression that may occur
         print(e)
 
-        
+# the function below has the goal to update an existing project after validating the input
+# it finally saves the changes
 @projects_bp.route('/update_project', methods=['GET', 'POST'])
 def update_project():   
     if request.method == 'POST':
@@ -229,10 +235,10 @@ def update_project():
         project_description = request.form.get('project_description')
         positions_needed = request.form.get('positions_needed')  
        # Process keywords and positions_needed into lists
-            # Para keywords
+            # for keywords
         keywords_str = f"['{', '.join([keyword.strip() for keyword in keywords.split(',')])}']" if keywords else "[]"
         keywords_list= keywords_str
-        # Para positions_needed
+        # for positions_needed
         positions_needed_str = f"['{', '.join([position.strip() for position in positions_needed.split(',')])}']" if positions_needed else "[]"
         positions_needed_list=positions_needed_str
 
@@ -271,24 +277,27 @@ def update_project():
         # Redirect to project listing 
         data_json =Project.load_all_projects_data(admin)       
         return render_template('Myprojects.html', data=data_json)
-   
+
+# the function below should retrieve the highest project id to ensure unique ids for new projects
 def get_max_id_project():
-    max_id = 0  # Inicializamos con 0, asumiendo que los IDs son números enteros
+    max_id = 0  #initialize with a zero assuming the ids are integers
     try:
         with open("generated_project_database.csv", mode="r", newline="", encoding="utf-8") as file:
             reader = csv.DictReader(file)
             for row in reader:
-                # Verificar si el campo 'id_project' está presente y es un número
+                # verify if the field 'id_project' is present and is a number
                 try:
-                    current_id = int(row['id_project'])  # Convertimos el id a un número
+                    current_id = int(row['id_project'])  #converts the id into a number 
                     if current_id > max_id:
-                         max_id = current_id  # Actualizamos el max_id si encontramos uno mayor
+                         max_id = current_id  #update the max_id if a higher one is found
                 except ValueError:
-                    continue  # Si no se puede convertir el valor a entero, se ignora
+                    continue  #if it not possible to convert the entire value, ignore it
     except FileNotFoundError:
         pass
     return max_id
 
+# the function below should allow users to search for projects using a query
+# also applying filters and sorting before displaying results 
 @projects_bp.route('/search_projects', methods=[ 'POST'])
 def search_projects():
     try:
@@ -319,6 +328,8 @@ def search_projects():
         # Return an error response if something goes wrong
         return jsonify({'error': 'An unexpected error occurred'}), 500
 
+# the function below should enable logged-in users to search their own projects using a query
+# with filters and sorting applied
 @projects_bp.route('/search_myprojects', methods=[ 'POST'])
 def search_myprojects():
     try:
